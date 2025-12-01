28109af6f6758c5e21ac70c620620cc789bed92f
Vachagan Petrosyan <vachagan.petrosyan@microchip.com>
2025-12-01 19:11:49 +0000

    Fixed telescope and added which-key plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,20 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "v0.1.9",
     dependencies = { 
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+            cond = function()
+                return vim.fn.executable 'make' == 1
+            end,
+        },
+        {
+            "nvim-telescope/telescope-ui-select.nvim"
+        },
+        {
+            "nvim-tree/nvim-web-devicons",
+            enabled = true
+        },
     }
 }
